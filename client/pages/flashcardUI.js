58b6186184f456c05a7ade3ea8e276823ab47438
angelinabiswas <angelinabiswas14@gmail.com>
2021-10-13 21:14:37 -0500

    Landing Page

---
+++ client/pages/flashcardUI.js
@@ -0,0 +1,4 @@
+import React, { Component } from 'react';
+import { EditorState, convertToRaw } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import '../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
