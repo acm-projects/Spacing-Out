86ba8aa8d85a5c331c174548b2991a2a64f1ea92
shreya6511 <shreyakuntumalla@gmail.com>
2021-11-08 22:16:34 -0600

    updated flashcard page after merge

--- client/pages/flashcards/[id].js
+++ client/pages/flashcards/[id].js
@@ -1,41 +1,59 @@
-import React, { Component, useEffect, useState } from 'react'
-import axios from 'axios'
-import { useRouter } from 'next/router'
+
+import React, { Component } from 'react'
+import 'rsuite/dist/rsuite.min.css';
 import { FlashcardComponent } from 'react-flashcard'
+import SideNavbar from '../../components/SideNavbar';
+import Container from 'rsuite/Container';
+import Header from 'rsuite/Header';
+import {useRouter} from 'next/router';
+import ButtonGroup from 'rsuite/ButtonGroup';
+import Button from 'rsuite/Button';
 
 const Page = () => {
-  const router = useRouter();
-  const { id } = router.query;
-  
-  let [cardData, setCardData] = useState([]);
-
-  useEffect(() => {
-    axios.get(`http://localhost:5000/flashcardsets/${id}/flashcards`)
-    .then( (response) => {
-       let tempCardData = response.data.map( (flashcard) => {
-          return (
-            {
-              front: {
-                text: flashcard.front
-              },
-              back: {
-                text: flashcard.back
-              }
-            }
-          );
-        });
-        setCardData(tempCardData);
-    })
-    .catch( (error) => {
-      console.log(error);
-    })
-    .then( () => {
-
-    });
-  }, [id]);
-  
-  return (<FlashcardComponent dataSource={cardData} />);
-
+  const router = useRouter()
+  const {id} = router.query
+    const cardData = [
+        {
+          front: {
+            text: "living outside, often in a tent",
+            image: "",
+          },
+          back: {
+            text: "Camping",
+          }
+        },
+        {
+          front: {
+            text: "A 1215 document that limited the king's ability to tax English nobles and that guaranteed due process and a right to trial",
+            image: "https://www.gannett-cdn.com/presto/2020/06/12/PCIN/f7da5daa-55ce-4c51-aeaa-92ca3afa7928-engraving-King-John-Magna-Carta-Runnymede-England-June-15-1215.jpg",
+          },
+          back: {
+            text: "Magna Carta",
+          }
+        }
+      ];
+    return (
+    <div className="show-fake-browser sidebar-page">
+    <Container>
+      <SideNavbar/>
+      <Container>
+            <Header>
+              <h2 style={{marginLeft: '4rem', marginTop: '2rem'}}>{id}</h2>
+            </Header>
+        <FlashcardComponent dataSource={cardData} />
+        <Container>
+        <ButtonGroup size="lg"  style={{ marginTop: 24, padding: 24, height: 12 }} justified>
+            <Button color="red" appearance="primary">      </Button>
+            <Button color="orange" appearance="primary">     </Button>
+            <Button color="yellow" appearance="primary">      </Button>
+            <Button color="cyan" appearance="primary">      </Button>
+            <Button color="green" appearance="primary">      </Button>
+        </ButtonGroup>
+        </Container>
+      </Container>
+    </Container>
+    </div>
+    );
 };
 
 export default Page; 
