d54e2cb7ae8d7d5242f89b464c6741b5b7256575
angelinabiswas <angelinabiswas14@gmail.com>
2021-11-10 21:23:31 -0600

    updated landing page, fix login form

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,49 +1,109 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import React from 'react';
-import { Button, ButtonGroup } from 'reactstrap';
-import styles from '../styles/Home.module.css'
+import Form from 'rsuite/Form';
+import Button from 'rsuite/Button';
+//mport ButtonToolbar from 'rsuite/ButtonToolbar';
+import { Navbar, Nav, Dropdown, Content, Sidebar, Grid, Row, Col, Modal, Footer } from 'rsuite';
+import { Container } from 'rsuite';
+import 'rsuite/dist/rsuite.min.css';
+import Home from '@rsuite/icons/Home';
 
-export default function Home() {
-  return (
+
+  export default function LandingPage() {
+    const [open, setOpen] = React.useState(false);
+    const [size, setSize] = React.useState();
+    const handleOpen = value => {
+      setSize(value);
+      setOpen(true);
+    };
+    const handleClose = () => setOpen(false);
+    const axios = require('axios');
+    //axios.post('/backend something', {Name: 'name', Email: 'email'} )
+      
+    return (
     
+    <Container>
+
+      <Navbar style={{backgroundColor:'#7660FF', appearance:'subtle', textDecoration: 'none', 
+              color:'black', fontSize:'1.15rem', height:'4rem'}}>
+          <Navbar.Brand style={{textDecoration: 'none', color:'white', fontWeight: 'heavy'}} icon={<Home></Home>}>
+            HOME
+          </Navbar.Brand>
+          <Nav pullRight>
+            <Nav.Item style={{textDecoration: 'none', padding: 'none', color:'white'}}>
+              Notes
+            </Nav.Item>
+            <Nav.Item style={{textDecoration: 'none', color:'white'}}>Flashcards</Nav.Item>
+            <Nav.Item style={{textDecoration: 'none', color:'black'}}> 
+              <Button size="med" onClick={() => handleOpen('xs')}>
+                Account
+              </Button>
+            </Nav.Item>
+            <Modal size={size} open={open} onClose={handleClose}>
+        <Modal.Header>
+          <Modal.Title>Login</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+            <Form>
+                <Form.Group controlId="name">
+                  <Form.ControlLabel>Name</Form.ControlLabel>
+                  <Form.Control name="name" type="name" />
+                </Form.Group>
+                <Form.Group controlId="email">
+                  <Form.ControlLabel>Email</Form.ControlLabel>
+                  <Form.Control name="email" type="email" />
+                </Form.Group>
+                <Form.Group controlId="password">
+                  <Form.ControlLabel>Password</Form.ControlLabel>
+                  <Form.Control name="password" type="password" autoComplete="off" />
+                </Form.Group>
+              </Form>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button>
+                New User?
+              </Button>
+
+              <Button onClick={handleClose} appearance="subtle">
+                Cancel
+              </Button>
+              
+              <Button onClick={handleClose} appearance="primary">
+                Submit
+              </Button>
+              
+            </Modal.Footer>
+          </Modal>
+        </Nav>
+      </Navbar>
     
-    <div className={styles.container}>
-      <Head>
+      <Head style={{color: "black"}}>
         <title>Spacing Out</title>
       </Head>
       
-      <main> 
-
-      <div className={styles.buttonGroup}>
-        <ButtonGroup color="#4B309C"> 
-          <Button>Dashboard</Button>
-          <Button>Notes</Button>
-          <Button>Flashcards</Button>
-        </ButtonGroup>
-      </div>
-
-        <div className={styles.title}>
-          SPACING OUT
-        </div>
-    
-        <div className={styles.description}>
-          A spaced repitition and note taking software
-        </div>
-
-        <div className={styles.description}>
-          for all your studying needs.
-        </div>
-        
-        <div>
+        <Row>
           
-          <Image src="/images/landingpicturenew.jpg" alt="Landing Picture"
-          width = "600"
-          height = "400" 
-          />
+          <h1 style={{textAlign:'center', alignItems: 'center',fontSize:'5rem', 
+          marginTop: '5rem', letterSpacing: '1.2rem'}}> SPACING OUT</h1>
+          
+        </Row>
 
-        </div>
-      </main>
-    </div>
+        <Row>
+
+          <h3 style={{textAlign:'center', alignItems:'center', marginTop:'3rem', fontWeight:'lighter' }}>
+             A spaced repitition and note taking software for all your studying needs.
+          </h3>
+
+        </Row>
+
+        <Row >
+        <Image styles={{alignItems: 'center'}} src="/images/landingpicturenew.jpg" alt="Landing Picture"
+          width = "600"
+          height = "400" />
+        </Row>
+
+    </Container>
   )
 }
+
